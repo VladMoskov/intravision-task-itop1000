391335bd84ec599cc6351429444ea0b5b987231f
VladMoskov <67361609+VladMoskov@users.noreply.github.com>
2021-04-22 19:29:16 +0300

    upload to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import {fromEvent, interval, Subject} from 'rxjs';
+import {buffer, debounceTime, filter, map, takeUntil, tap} from "rxjs/operators";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export function App() {
+
+    const wait = useRef(null);
+    const [isPaused, setIsPaused] = useState(false);
+    const [ticker, setTicker] = useState(0);
+    const [isStarted, setIsStarted] = useState(false);
+
+
+
+    useEffect(() => {
+
+        const click$ = fromEvent(wait.current, 'click');
+        const doubleClick$ = click$.pipe(
+            buffer(click$.pipe(debounceTime(300))),
+            map(clicks => clicks.length),
+            filter(length => length === 2),
+            tap(() => setIsPaused(true))
+        )
+
+        const subscribe$ = new Subject();
+        interval(1000)
+            .pipe(
+                takeUntil(subscribe$),
+                takeUntil(doubleClick$)
+            )
+            .subscribe(() => {
+                !isPaused && isStarted && setTicker(v => v + 1000)
+            });
+
+        return () => {
+            subscribe$.next();
+            subscribe$.complete();
+        };
+    }, [isStarted, isPaused]);
+
+    const start = () => {
+        if (isPaused) {
+            setIsPaused(false)
+        } else {
+            setIsStarted(!isStarted);
+            setTicker(0)
+        }
+    }
+
+    const reset = () => {
+        setTicker(0);
+        setIsPaused(false)
+    }
+
+    return <div>
+
+        <h1>{new Date(ticker).toISOString().slice(11, 19)}</h1>
+
+        <button onClick={start}>Start/Stop</button>
+        <button ref={wait}>Wait</button>
+        <button onClick={reset}>Reset
+        </button>
     </div>
-  );
 }
-
-export default App;
